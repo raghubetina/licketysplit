702d2139c33adc8de0176148a81764c0d32a116f
Raghu Betina <raghu@firstdraft.com>
2026-07-10 18:22:09 -0500

    Normalize application.js to Prettier style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "turbo-refresh-animations"
-import "./controllers"
+import '@hotwired/turbo-rails'
+import 'turbo-refresh-animations'
+import './controllers'
 
 // Ahoy analytics tracking
-import ahoy from "ahoy.js"
+import ahoy from 'ahoy.js'
 
 // Track page views
-ahoy.trackView();
+ahoy.trackView()
 
 // Example: Track custom events
 // ahoy.track("Clicked Button", {button: "signup"});
